f00a074d466eaaf9a3178973bd038ced03bee1ed
Sergio Rua <58211930+digiserg@users.noreply.github.com>
2025-05-10 20:50:46 -0500

    Debian (#1)
    Debian and OSX

--- package.sh
+++ package.sh
@@ -7,7 +7,11 @@ PKG_VERSION=${1:-0.0.5}
 GEM_USER_DIR=$(ruby -r rubygems -e 'puts Gem.user_dir')
 
 # Construct the bin path
-GEM_BIN_DIR="$GEM_USER_DIR/bin"
+if command -v fpm >/dev/null 2>&1; then
+  GEM_BIN_DIR=$(dirname $(command -v fpm))
+else
+  GEM_BIN_DIR="$GEM_USER_DIR/bin"
+fi
 
 export PATH=$PATH:$GEM_BIN_DIR
 
@@ -29,6 +33,7 @@ if [ -f /etc/os-release ]; then
     TARGET="rpm"
   fi
   PKG_VERSION=${PKG_VERSION}~${ID}${VERSION_ID}
+  LIB_DIR=$(ls -1 build/ | grep lib.linux)
 
   rm -f axonops-cqlsh_${PKG_VERSION}_${ARCH}.deb
   fpm -s dir -t $TARGET -n axonops-cqlsh -v ${PKG_VERSION} -a $ARCH \
@@ -38,14 +43,14 @@ if [ -f /etc/os-release ]; then
     -d libpython3.11 \
     --prefix /opt/AxonOps \
     axonops-cqlsh=/bin/cqlsh \
-    build/lib.linux-$(uname -m)-cpython-311/=/lib
+    build/${LIB_DIR}/=/lib
 fi
 
 if [ "$(uname -s)" == "Darwin" ]; then
-  PKG_VERSION=${PKG_VERSION}~darwin${redhat_version}
   ARCH=$(uname -m)
   MAJOR_VERSION=$(sw_vers -productVersion | cut -d '.' -f 1)
   LIB_DIR=$(ls -1 build/ | grep lib.)
+  PKG_VERSION=${PKG_VERSION}~osx${MAJOR_VERSION}
 
   fpm -s dir -t zip -n axonops-cqlsh-${PKG_VERSION}-${ARCH} -v ${PKG_VERSION} -a $ARCH \
     --maintainer "AxonOps Limited <support@axonops.com>" \
